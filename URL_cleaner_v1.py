ec9ac2dbff88c511d3c591bdf23cf9f22ee5efb9
nloyan <106363449+nloyan@users.noreply.github.com>
2022-06-15 22:44:00 -0400

    Update URL_cleaner_v1.py
    removed personal details

--- URL_cleaner_v1.py
+++ URL_cleaner_v1.py
@@ -2,7 +2,7 @@
 
 from urllib.parse import urlparse
 
-with open("C:/Users/Nloya/desktop/url2.txt") as text_data:
+with open("C:/Users/xxxx/desktop/url2.txt") as text_data:
     for line in text_data:
         parsed_url = urlparse(line)
         cleaned_url = parsed_url.netloc
