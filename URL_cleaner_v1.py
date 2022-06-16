efa3a7b5b0833938cb1422ff2419fa49049fc3a4
nloyan <106363449+nloyan@users.noreply.github.com>
2022-06-15 22:43:30 -0400

    Add files via upload
    v1 of url cleaner

---
+++ URL_cleaner_v1.py
@@ -0,0 +1,13 @@
+# simple URL cleaner for large lists
+
+from urllib.parse import urlparse
+
+with open("C:/Users/Nloya/desktop/url2.txt") as text_data:
+    for line in text_data:
+        parsed_url = urlparse(line)
+        cleaned_url = parsed_url.netloc
+        url_fixed = cleaned_url.replace("[.]", ".")
+        url_fixed2 = url_fixed.replace("www.", "")
+        print(url_fixed2)
+
+text_data.close()
