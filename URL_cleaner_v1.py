cd0373a031145f8d4f8b795c0f06c42bb6991630
Nur Loyan <106363449+nloyan@users.noreply.github.com>
2022-06-15 23:52:35 -0400

    Update URL_cleaner_v1.py

--- URL_cleaner_v1.py
+++ URL_cleaner_v1.py
@@ -1,14 +1,15 @@
 # simple URL cleaner for large lists
 
-from urllib.parse import urlparse
+import urllib.parse
 
-with open("C:/Users/xxxx/desktop/url.txt") as text_data:
+with open("C:/Users/xxxx/desktop/urls.txt") as text_data:
     for line in text_data:
-        parsed_url = urlparse(line)
+        parsed_url = urllib.parse.urlparse(line)
         cleaned_url = parsed_url.netloc
         url_fixed = cleaned_url.replace("[.]", ".")
         final_urls = url_fixed.replace("www.", "")
         print(final_urls)
-        text_data.close()
+        
+text_data.close()
 
 
